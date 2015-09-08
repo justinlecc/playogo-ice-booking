4b99d8637e64b338b6df1ae56375c6c3853b1e2c
Justin Leclerc <justin_leclerc@hotmail.com>
2015-09-08 16:50:30 -0400

    hdkfj

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # JL edit to remove 404 on image assets: config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
