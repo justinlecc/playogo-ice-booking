87f037160a10777847f9a4a4784416c8531f69c3
Justin Leclerc <justin_leclerc@hotmail.com>
2016-03-08 07:33:23 -0500

    added tracking to the about and contact page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,7 +2,12 @@ class AboutController < ApplicationController
 
   # GET /about
   def index
-
+    # Track page view
+    source = params[:src] # TODO: Escape src
+    v = Viewer.new
+    info = v.pageView('/about', source, cookies)
+    cookies[:viewer_id]    = info[:viewer_id]
+    cookies[:page_view_id] = info[:page_view_id]
   end
 
 end
--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,8 +1,13 @@
 class ContactController < ApplicationController
 
-  # GET /about
-  def index
-
-  end
+    # GET /about
+    def index
+        # Track page view
+        source = params[:src] # TODO: Escape src
+        v = Viewer.new
+        info = v.pageView('/contact', source, cookies)
+        cookies[:viewer_id]    = info[:viewer_id]
+        cookies[:page_view_id] = info[:page_view_id]
+    end
 
 end
